3a5770bb62898dfa81a841dc5d754f6c6f4b085e
christopher-koleda <koleda.christopher@gmail.com>
2018-05-14 20:09:14 -0400

    Add ThreadPolicy.php and include it in AuthServiceProvider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use App\Thread;
+use App\Policies\ThreadPolicy;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        'App\Model' => 'App\Policies\ModelPolicy',
+        Thread::class => ThreadPolicy::class,
     ];
 
     /**
