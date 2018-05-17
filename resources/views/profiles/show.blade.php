e0df000e81bba1d3389d7bf8924db4b304254222
christopher-koleda <koleda.christopher@gmail.com>
2018-05-17 01:46:00 -0400

    Refactor profiles.show activity feed

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -9,13 +9,13 @@
 @section('content')
 
   <div class="columns">
-    <div class="column">
-      <div class="lu-card tw-pb-4">
+    <div class="column is-4-desktop">
+      <div class="lu-card tw-pb-4 offset-t">
         <div class="lu-card-header tw-px-4 tw-text-center">
           {{ $user->name }}
         </div>{{-- end header --}}
 
-        <div class="tw-px-4 tw-pt-4">
+        <div class="tw-px-4 tw-pt-4 tw-mx-auto">
           <img src="https://imgplaceholder.com/420x320" alt="profile picture">
         </div>{{-- end profile pic --}}
 
@@ -48,16 +48,19 @@
             </div>
           </li>
         </ul>{{-- end icons section --}}
-        
-        <hr class="tw-mx-4 tw-mt-4 tw-mb-0">
+      </div>{{-- end lu-card  --}}
+    </div>{{-- end info widget column --}}
 
-        <h1 class="tw-mb-2 tw-text-center tw-text-2xl tw-font-light">
-          Recent Activity
-        </h1>
+    <div class="column is-offset-1-desktop">
 
+      <h1 class="tw-mb-2 tw-text-center tw-text-2xl tw-font-light">
+        Recent Activity
+      </h1>{{-- end feed header --}}
+
+      <div class="lu-card tw-py-4 tw-mb-4">
         <ul class="tw-px-6">
           @foreach($activities as $date => $activity)
-          <span class="tw-text-xs">{{$date}}</span>
+            <span class="tw-text-xs">{{$date}}</span>
             @foreach($activity as $record)
               <li>
                 @include("profiles.activities.{$record->type}", ['activity' => $record])
@@ -65,14 +68,13 @@
             @endforeach
           @endforeach
         </ul> 
-        
-        {{-- end activity feed --}}
+      </div>{{-- end activity feed widget --}}
 
 
-      </div>{{-- end lu-card  --}}
-    </div>{{-- end info widget column --}}
-
-    <div class="column is-8">
+      
+      <h1 class="tw-mb-2 tw-text-center tw-text-2xl tw-font-light">
+        Top Threads
+      </h1>
 
       <div class="lu-pannel">
         @foreach($threads as $thread)
@@ -82,12 +84,12 @@
             <h4 class="tw-font-light">{{ $thread->title }}</h4>
           </a>
         </div>
-
         <div class="lu-pannel-body">
           {{$thread->body}}
         </div>
+        
         <div class="lu-level">
-           
+      
           <div class="lu-level-item tw-text-grey-darker ">
             <i class="fas fa-clock tw-mr-1 tw-text-grey-darker tw-align-middle"></i>
             <span class="tw-align-middle">
