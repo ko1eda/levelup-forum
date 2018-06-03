f446bb6dd6ebb6ab4c0074d0b6008bf665696fa1
christopher-koleda <koleda.christopher@gmail.com>
2018-06-02 23:57:24 -0400

    Fix reply.blade.php spacing issues

--- resources/views/threads/partials/reply.blade.php
+++ resources/views/threads/partials/reply.blade.php
@@ -1,38 +1,36 @@
 
 <div class="tw-flex tw-flex-col tw-w-full tw-border tw-border-bulma ">
-  <div class="tw-py-2 sm:tw-px-4 tw-px-2 tw-border-b tw-bg-bulma-lightest">
 
+  <div class="tw-py-2 sm:tw-px-4 tw-px-2 tw-border-b tw-bg-bulma-lightest">
     <div class="tw-flex tw-justify-between tw-items-center">
-      <a href="{{ route('profiles.show', $reply->user) }}" class="username tw-py-2">
+      <a href="{{ route('profiles.show', $reply->user) }}" class="username">
         <h1 class="sm:tw-text-base tw-text-sm tw-font-light">
           {{ $reply->user->name }}
         </h1>
-      </a>
+      </a>{{-- end header left-side (username) --}}
       
       @can('delete', $reply)
-        <form class="tw-mr-1" action="{{ route('replies.destroy', $reply) }}" method="POST">
+        <form class="tw-mr-1 tw-flex tw-items-center" action="{{ route('replies.destroy', $reply) }}" method="POST">
             @csrf
             @method('delete')
           <button class="delete is-small hover:tw-bg-red-light"></button>
         </form>
-      @endcan
-      {{-- only show the delete button for the user who owns the reply --}}
-      
-    </div>
+      @endcan{{-- only show the delete button for the user who owns the reply --}}
+    </div>{{-- end level --}}
   </div>{{-- end header --}}
 
-  <div class="sm:tw-px-4 tw-px-2 tw-pt-2 sm:tw-text-base tw-text-sm tw-leading-loose">
-    {{ $reply->body }}
-  </div>{{-- end body --}}
-
-  <div class="sm:tw-px-4 tw-px-2 tw-pb-2 tw-flex tw-items-center ">
-    <div class="tw-mr-2 tw-text-xs sm:tw-text-sm ">
-      {{ $reply->created_at->diffForHumans() }}
-    </div>{{-- end time --}}
+  <div class="sm:tw-px-4 tw-px-2 tw-py-2">
+    <div class=" tw-mb-1 sm:tw-text-base tw-text-sm">
+      {{ $reply->body }}
+    </div>{{-- end body text --}}
+  
+    <div class="tw-flex tw-items-center ">
+      <div class="tw-mr-2 tw-text-xs sm:tw-text-sm ">
+        {{ $reply->created_at->diffForHumans() }}
+      </div>{{-- end time --}}
 
       <form class="tw-mr-1" action="/replies/{{$reply->id}}/favorites" method="POST">
-        @csrf
-        
+        @csrf     
         @if($reply->isFavorited())
           <button class="tw-flex tw-items-center" type="submit" disabled>
             <i class="fas fa-star tw-text-xs tw-text-yellow-dark"></i>
@@ -42,14 +40,15 @@
             <i class="far fa-star tw-text-xs tw-text-bulma-dark hover:tw-text-yellow-dark"></i>
           </button>
         @endif
+      </form>{{-- end favorites button --}}
 
-      </form>
-  
-    <div class="tw-text-xs sm:tw-text-sm ">
-      {{ $reply->favorites_count }}
-    </div>
+      <div class="tw-text-xs sm:tw-text-sm ">
+        {{ $reply->favorites_count }}
+      </div>{{-- end favorites count --}} 
 
-  </div>{{-- end info bar --}}
+    </div>{{-- end info level --}}
+  </div>{{-- end reply body --}}
+
 </div>
 
 
