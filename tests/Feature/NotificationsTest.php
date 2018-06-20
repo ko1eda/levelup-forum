c9e9452419bf60939b9e45fe7e7a94f96a01b8b2
christopher-koleda <koleda.christopher@gmail.com>
2018-06-19 21:07:42 -0400

    Refactor NotificationsTest to use factory class, all tests passing

--- tests/Feature/NotificationsTest.php
+++ tests/Feature/NotificationsTest.php
@@ -7,22 +7,27 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Thread;
 use App\User;
+use Illuminate\Notifications\DatabaseNotification;
 
 class NotificationsTest extends TestCase
 {
     use RefreshDatabase;
 
-    
+    public function setUp()
+    {
+        parent::setup();
+
+        $this->signInUser();
+    }
+
+
     /** @test */
     public function a_notification_is_prepared_when_a_subscribed_thread_recieves_a_new_reply_not_by_the_current_user()
     {
-        // Given we have a user
-        $this->signInUser();
+        // Given we have a user (boot method)
 
-        // And a thread
-        $thread = factory(Thread::class)->create();
-        
         // Which our user is subscribed to
+        $thread = factory(Thread::class)->create();
         $thread->addSubscription();
 
        // If that user replies to the thread they are subscribed to
@@ -47,33 +52,22 @@ class NotificationsTest extends TestCase
 
     /** @test */
     public function a_user_can_mark_a_single_notification_as_read()
-    { 
-        // Given we have a user
-        $this->signInUser();
-    
-        // And a thread
-        $thread = factory(Thread::class)->create();
-            
-        // Which our user is subscribed to
-        $thread->addSubscription();
-    
-        // If another user replies to the thread they are subscribed to
-        for ($i = 0; $i < 2; $i++) {
-            $thread->addReply([
-                'user_id' => factory(User::class)->create()->id,
-                'body' => 'I am the replies body'
-            ]);
-        }
+    {
+        // Given we have a user (boot method above)
+        // and a thread which our user is subscribed to
 
-        $firstNotification = \Auth::user()->unreadNotifications()->first();
-    
-        // And the user recieves 2 notifications
+        // If the user recieves 2 notifications
+        $notifications = factory(DatabaseNotification::class, 2)->create();
+
+        // Then that user should have 2 unread notifications
         $this->assertEquals(2, \Auth::user()->unreadNotifications()->count());
-    
-        // Then if that user hits the endpoint and removes the last notification
+
+        // And then when that user hits the endpoint and removes one of the notifications
+        $firstNotification = \Auth::user()->unreadNotifications()->first();
+
         $this->json('PATCH', route('users.notifications.update', [\Auth::user(), $firstNotification->id]));
     
-        // Then the notification should be marked as read and there should only be one unread
+        // Then there should only be one unread notification remaining
         $this->assertEquals(1, \Auth::user()->unreadNotifications()->count());
     }
 
@@ -81,22 +75,12 @@ class NotificationsTest extends TestCase
     /** @test */
     public function a_user_can_mark_all_notifications_as_read()
     {
-        // Given we have a user
-        $this->signInUser();
-    
-        // And a thread
-        $thread = factory(Thread::class)->create();
-            
-        // Which our user is subscribed to
-        $thread->addSubscription();
-    
-        // If another user replies to the thread they are subscribed to
-        for ($i = 0; $i < 2; $i++) {
-            $thread->addReply([
-                'user_id' => factory(User::class)->create()->id,
-                'body' => 'I am the replies body'
-            ]);
-        }
+
+        // Given we have a user (boot method above)
+        // and a thread which our user is subscribed to
+
+        // If the user recieves 2 notifications
+        $notifications = factory(DatabaseNotification::class, 2)->create();
 
         // And the user sends a patch request to our endpoint WITHOUT the notificationID
         // wildcard
@@ -111,52 +95,30 @@ class NotificationsTest extends TestCase
     /** @test */
     public function a_user_can_fetch_all_notifications()
     {
-        // Given we have an authenticated user
-        $this->signInUser();
-    
-        // And a thread
-        $thread = factory(Thread::class)->create();
-            
-        // Which our user is subscribed to
-        $thread->addSubscription();
-    
-        // if that thread gets three new replies
-        for ($i = 0; $i < 3; $i++) {
-            $thread->addReply([
-                'user_id' => factory(User::class)->create()->id,
-                'body' => 'I am the replies body'
-            ]);
-        }
-    
+        // Given we have a user (boot method above)
+        // and a thread which our user is subscribed to
+
+        // If the user recieves 3 notifications
+        $notifications = factory(DatabaseNotification::class, 3)->create();
+
         // Then the user should have 3 notifications
         $this->assertCount(3, \Auth::user()->notifications);
     
         // And that user sends an ajax request to our endpoint
-        // Then it should return 3 notifications
+        // Then it should return all 3 notifications
         $this->json('get', route('users.notifications.index', \Auth::user()))
             ->assertJsonCount(3, '');
     }
 
 
     /** @test */
-    public function a_user_can_fetch_unread_notifications()
+    public function a_user_can_fetch_all_unread_notifications()
     {
-        // Given we have an authenticated user
-        $this->signInUser();
-    
-        // And a thread
-        $thread = factory(Thread::class)->create();
-     
-        // Which our user is subscribed to
-        $thread->addSubscription();
+       // Given we have a user (boot method above)
+        // and a thread which our user is subscribed to
 
-        // if that thread gets three new replies
-        for ($i = 0; $i < 3; $i++) {
-            $thread->addReply([
-                'user_id' => factory(User::class)->create()->id,
-                'body' => 'I am the replies body'
-            ]);
-        }
+        // If the user recieves 3 notifications
+        $notifications = factory(DatabaseNotification::class, 3)->create();
 
         // Then the user should have 3 notifications
         $this->assertCount(3, \Auth::user()->notifications);
