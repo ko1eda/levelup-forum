624bc7c0408703cecaf3510ea909e5b7879dccc9
christopher-koleda <koleda.christopher@gmail.com>
2018-07-05 23:00:46 -0400

    Fix FlashMessage component to include parser function
    This function will allow for flash components that come from laravel
    to have the ability to set their flash levels using string parsing
    in the format 'message~level'.

--- resources/assets/js/components/Flash/FlashMessage.vue
+++ resources/assets/js/components/Flash/FlashMessage.vue
@@ -9,64 +9,105 @@
 </template>
 
 <script>
-  export default {
-    props: {
-      message: {
-        type: String,
-      }
+export default {
+  props: {
+    message: {
+      type: String
+    }
+  },
+
+  data() {
+    return {
+      display: false,
+      body: this.message,
+      level: "primary"
+    };
+  },
+
+  methods: {
+    /**
+     * parses flash message string splitting
+     * any laravel related flash messages by tilde
+     * in the format ->with('flash', 'message~level')
+     *
+     * If the string contains no ~ (has a length of 1) it is assumed to
+     * have been called through the our javascript flash function
+     * and is therefore returned without setting the level -- as it would
+     * have already been set when the flashEvent event was fired
+     * 
+     */
+    parseSessionFlash(message) {
+      let stringArray = message.split("~");
+
+      if (stringArray.length === 1) return stringArray[0];
+      
+      this.setFlashLevel(stringArray[1]);
+      
+      return stringArray[0];
     },
 
-    data() {
-      return {
-        display: false,
-        body: this.message,
-        level: 'primary'
-      }
+
+
+    /**
+     * sets the flash level (aka color of the message).
+     */
+    setFlashLevel(level) {
+      this.level = level.toLowerCase();
     },
 
-    methods: {
 
-      // set the body attribute to the 
-      // incoming flash message value
-      // display the notification
-      // then set the hide timer
-      show(message) {
-        this.body = message;
 
-        this.display = true
-
-        this.hide();
-      },
-
-      // Dismiss the message after
-      // 3 seconds
-      hide() {
-        setTimeout(() => {
-          this.display = false
-        }, 3000);
-      },
+    /**
+     * Display the flash message after it is parsed
+     * then hide the message.
+     */
+    show(message) {
+      this.body = this.parseSessionFlash(message);
+      this.display = true;
+      this.hide();
     },
 
-    // on a flash event from the global Vue eventbus
-    // show the flash message
+
+
+    // Dismiss the message after
+    // 3 seconds
+    hide() {
+      setTimeout(() => {
+        this.display = false;
+      }, 3000);
+    }
+  },
+
+
+
+    /**
+     * If a message exists from our laravel end, first display that message
+     * 
+     * Next add an event listener to listen for any flash events emitted from our global Vue eventbus.
+     * If a level has been set, use that level then display the message.
+     * 
+     * If the level has not been set, set it to primary (default)
+     */
     created() {
-      if(this.message)
-        this.show(this.message);
-    
-      window.events.$on('flashEvent', ({message, level = null}) => {
-        level && (this.level = level);
+      if (this.message) this.show(this.message);
+
+      window.events.$on("flashEvent", ({ message, level = null }) => {
+        level 
+          ? this.setFlashLevel(level) 
+          : this.setFlashLevel('primary');
+
         this.show(message);
       });
-    }
-
-  }
+    },
+};
 </script>
 
 
+
 <style lang="scss" scoped>
-  .pos {
-    position: fixed;
-    bottom: .25rem;
-    right: 0
-  }
+.pos {
+  position: fixed;
+  bottom: 0.25rem;
+  right: 0;
+}
 </style>
