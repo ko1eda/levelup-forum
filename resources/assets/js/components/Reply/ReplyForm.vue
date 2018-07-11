d7f5c157f0efd3d35cf58be2c9717b3bc196a186
christopher-koleda <koleda.christopher@gmail.com>
2018-07-11 00:17:18 -0400

    Basic vue-at implementation working

---
+++ resources/assets/js/components/Reply/ReplyForm.vue
@@ -0,0 +1,18 @@
+
+<script>
+import AtTa from 'vue-at/dist/vue-at-textarea'
+
+export default {
+
+  components: {
+    AtTa
+  },
+
+  data() {
+    return {
+      members: ['Roxie Miles', 'grace.carroll', '小浩'],
+    }
+  }
+
+}
+</script>
--- resources/views/threads/partials/reply-form.blade.php
+++ resources/views/threads/partials/reply-form.blade.php
@@ -1,35 +1,40 @@
-<form action="{{ route('replies.store', $thread) }}" method="POST">
-  {{ csrf_field() }}
-  
-  <div class="field">
-    <label class="label">
-      <h1 class="tw-text-lg">
-        Reply:
-      </h1>
-    </label>
+<lu-reply-form inline-template>
+    <form action="{{ route('replies.store', $thread) }}" method="POST">
+      {{ csrf_field() }}
+      
+      <div class="field">
+        <label class="label">
+          <h1 class="tw-text-lg">
+            Reply:
+          </h1>
+        </label>
+        
+        <div class="control">
+          <at-ta :members="members">
+            <textarea class="textarea" name="body"></textarea>
+          </at-ta>
+        </div>
+      </div>
+      
+      <div class="field">
+        <div class="control">
     
-    <div class="control">
-      <textarea class="textarea" name="body"></textarea>
-    </div>
-  </div>
+          <p class="help is-danger">
+            @if(count($errors->all()))
+              {{ $errors->first() }}  
+            @endif
+          </p>
+          {{-- end spam dection error check --}}
+    
+        </div>
+      </div>
+    
+      <div class="field">
+        <div class="control">
+          <button type="submit" class="button is-small is-outlined is-primary">Submit</button>
+        </div>
+      </div>
+    
+    </form>
   
-  <div class="field">
-    <div class="control">
-
-      <p class="help is-danger">
-        @if(count($errors->all()))
-          {{ $errors->first() }}  
-        @endif
-      </p>
-      {{-- end spam dection error check --}}
-
-    </div>
-  </div>
-
-  <div class="field">
-    <div class="control">
-      <button type="submit" class="button is-small is-outlined is-primary">Submit</button>
-    </div>
-  </div>
-
-</form>
+</lu-reply-form>
