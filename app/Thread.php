639bd06be17a7104865aec503893ead6084b15b4
christopher-koleda <koleda.christopher@gmail.com>
2018-08-09 01:52:30 -0400

    When a thread is deleted its views are removed, test passed

--- app/Thread.php
+++ app/Thread.php
@@ -69,6 +69,9 @@ class Thread extends Model implements SubscribableInterface
             $thread->replies->each(function ($reply) {
                 $reply->delete();
             });
+
+            // Clear the threads views from the redis cache
+            $thread->views()->clear();
         });
     }
 
