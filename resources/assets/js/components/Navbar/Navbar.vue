1ec2d709887abf4bfcc29be48cd1a67a61b141dd
christopher-koleda <koleda.christopher@gmail.com>
2018-08-11 21:42:31 -0400

    Add navbar toggle functionality and display notifications to right --fixes #2, #20

---
+++ resources/assets/js/components/Navbar/Navbar.vue
@@ -0,0 +1,37 @@
+<script>
+import NotificationWidget from '../Notification/NotificationWidget'
+export default {
+
+  components: {
+     'lu-notification-widget': NotificationWidget,
+  },
+
+
+  data() {
+    return {
+
+      isActive: false
+
+    }
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+//  Anything above a tablets max portrait width
+//  will not have the second notification widget visible
+@media (min-width: 1025px) {
+  .hidden-non-touch {
+    display:none;
+  }
+}
+// this hides the original notification widget
+// on anything below 1025px
+@media (max-width: 1025px) {
+  .hidden-touch {
+    display:none;
+  }
+}
+</style>
+  
--- resources/views/shared/navbar.blade.php
+++ resources/views/shared/navbar.blade.php
@@ -1,96 +1,109 @@
-<nav class="navbar tw-shadow" role="navigation">
-  <div class="container">
-    <div class="navbar-brand">
-      <a class="navbar-item" href="{{ url('/') }}">
-        {{ config('app.name') }}
-      </a>
-      <div class="navbar-burger burger" data-target="">
-        <span></span>
-        <span></span>
-        <span></span>
+<lu-navbar inline-template>
+  <nav class="navbar tw-shadow" role="navigation">
+    <div class="container">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="{{ url('/') }}">
+          {{ config('app.name') }}
+        </a>
+        <div :class="['navbar-burger burger', {'is-active' : isActive }]"  @click="isActive ? isActive = false :  isActive = true">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
       </div>
-    </div>
-
-    <div id="navbar" class="navbar-menu">
-
-      <div class="navbar-start">
-        
-        <div class="navbar-item has-dropdown is-hoverable ">
-          <a class="navbar-link">
-            Browse
-          </a>
-          <div class="navbar-dropdown is-boxed">
-            <a class="navbar-item" href="{{ route('threads.index') }}">All Threads</a>
-            <a class="navbar-item" href="{{ route('threads.index', '?popular=1') }}">Popular Threads</a>
-            <a class="navbar-item" href="{{ route('threads.index', '?active=1') }}">Active Threads</a>
-            <a class="navbar-item" href="{{ route('threads.index', '?unresponded=1') }}">Unresponded Threads</a>
-          </div>
-        </div>{{-- Browse dropdown --}}
-
-        <div class="navbar-item has-dropdown is-hoverable ">
-          <a class="navbar-link">
-            Channels
-          </a>
-          <div class="navbar-dropdown is-boxed">
-            @foreach ($channels as $channel)
-            <a class="navbar-item" href="{{ route('threads.index', $channel) }}">
-              {{$channel->name}}
-            </a>
-            @endforeach
-          </div> 
-        </div>{{-- Channels dropdown --}}
-
-      </div>{{-- end navbar-start --}}
-
-      <div class="navbar-end">
-        @guest
-        <a class="navbar-item" href="{{ route('login') }}">{{ __('Login') }}</a>
-        <a class="navbar-item" href="{{ route('register') }}">{{ __('Register') }}</a>
-        @else
-          @if(count(Auth::user()->unreadNotifications))
-            <lu-notification-widget 
-                :user-data="{{ \Auth::user()->makeHidden('email') }}"
-                :index-route={{ json_encode(route('users.notifications.index', \Auth::user(), false )) }}
-                :mark-route={{ json_encode(route('users.notifications.update', \Auth::user(), false)) }}>
-            </lu-notification-widget>
-          @endif
-          {{-- end notifications vue component --}}
-
+  
+      <div id="navbar" :class="['navbar-menu', {'is-active' : isActive }, {'tw-border-t tw-border-bulma-lighter' : isActive}]">
+  
+        <div class="navbar-start">
+          
           <div class="navbar-item has-dropdown is-hoverable ">
+            {{-- this just moves the notification widget up higher in the mobile drop down menu --}}
+            {{-- it is completely hidden on anything with a screen size above 1025px --}}
+            <lu-notification-widget v-if="isActive" class="hidden-non-touch tw-border-b tw-border-bulma-lighter"
+            :user-data="{{ \Auth::user()->makeHidden('email') }}"
+            :index-route={{ json_encode(route('users.notifications.index', \Auth::user(), false )) }}
+            :mark-route={{ json_encode(route('users.notifications.update', \Auth::user(), false)) }}
+            :navbar-Active="isActive">
+          </lu-notification-widget>
+
             <a class="navbar-link">
-              {{ Auth::user()->name }}
+              Browse
             </a>
             <div class="navbar-dropdown is-boxed">
-              <a href="{{ route('threads.create') }}" class="navbar-item">
-                New Thread
-              </a>
-              <a class="navbar-item" href="{{ route('profiles.show', Auth::user()) }}">
-                My Profile
-              </a>
-              <a class="navbar-item" href="/threads/?by={{ Auth::user()->name }}">
-                My Threads
-              </a>
-
-              <div class="navbar-divider"></div>
-
-              <a class="navbar-item" href="{{ route('profiles.settings.edit', Auth::user()) }}">
-                 Settings
-              </a>{{-- profile settings --}}
-
-              <a class="navbar-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                  document.getElementById('logout-form').submit();">
-                {{ __('Logout') }}
-              </a>
-              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                @csrf
-              </form>
-
+              <a class="navbar-item" href="{{ route('threads.index') }}">All Threads</a>
+              <a class="navbar-item" href="{{ route('threads.index', '?popular=1') }}">Popular Threads</a>
+              <a class="navbar-item" href="{{ route('threads.index', '?active=1') }}">Active Threads</a>
+              <a class="navbar-item" href="{{ route('threads.index', '?unresponded=1') }}">Unresponded Threads</a>
             </div>
-          </div>{{-- User dropdown --}}
-
-        @endguest
-      </div>{{-- navbar-end ends --}}
-      
+          </div>{{-- Browse dropdown --}}
+          
+          <div class="navbar-item has-dropdown is-hoverable ">
+            <a class="navbar-link">
+              Channels
+            </a>
+            <div class="navbar-dropdown is-boxed">
+              @foreach ($channels as $channel)
+              <a class="navbar-item" href="{{ route('threads.index', $channel) }}">
+                {{$channel->name}}
+              </a>
+              @endforeach
+            </div> 
+          </div>{{-- Channels dropdown --}}
+  
+        </div>{{-- end navbar-start --}}
+  
+        <div class="navbar-end">
+          @guest
+          <a class="navbar-item" href="{{ route('login') }}">{{ __('Login') }}</a>
+          <a class="navbar-item" href="{{ route('register') }}">{{ __('Register') }}</a>
+          @else
+            @if(count(Auth::user()->unreadNotifications))
+              <lu-notification-widget class="hidden-touch"
+                  :user-data="{{ \Auth::user()->makeHidden('email') }}"
+                  :index-route={{ json_encode(route('users.notifications.index', \Auth::user(), false )) }}
+                  :mark-route={{ json_encode(route('users.notifications.update', \Auth::user(), false)) }}
+                  :navbar-Active="isActive"
+                  >
+              </lu-notification-widget>
+            @endif
+            {{-- end notifications vue component --}}
+  
+            <div class="navbar-item has-dropdown is-hoverable ">
+              <a class="navbar-link">
+                {{ Auth::user()->name }}
+              </a>
+              <div class="navbar-dropdown is-boxed">
+                <a href="{{ route('threads.create') }}" class="navbar-item">
+                  New Thread
+                </a>
+                <a class="navbar-item" href="{{ route('profiles.show', Auth::user()) }}">
+                  My Profile
+                </a>
+                <a class="navbar-item" href="/threads/?by={{ Auth::user()->name }}">
+                  My Threads
+                </a>
+  
+                <div class="navbar-divider"></div>
+  
+                <a class="navbar-item" href="{{ route('profiles.settings.edit', Auth::user()) }}">
+                   Settings
+                </a>{{-- profile settings --}}
+  
+                <a class="navbar-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                    document.getElementById('logout-form').submit();">
+                  {{ __('Logout') }}
+                </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                  @csrf
+                </form>
+  
+              </div>
+            </div>{{-- User dropdown --}}
+  
+          @endguest
+        </div>{{-- navbar-end ends --}}
+        
+      </div>
     </div>
-  </div>
-</nav>
+  </nav>
+</lu-navbar>
