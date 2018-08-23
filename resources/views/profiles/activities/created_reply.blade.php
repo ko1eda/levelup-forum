ac6ddac8840e861668e68d8f68d49649bff25e51
christopher-koleda <koleda.christopher@gmail.com>
2018-08-23 01:57:40 -0400

    Temporary fix for reply activity in activity feed displaying html

--- resources/views/profiles/activities/created_reply.blade.php
+++ resources/views/profiles/activities/created_reply.blade.php
@@ -14,35 +14,9 @@
     @endslot
 
     @slot('body')
-      {{$activity->subject->body}}
+      {{ strip_tags(substr($activity->subject->body, 0, 50)) . '...' }}
     @endslot
 
   @endcomponent
 
 @endif
-
-  {{-- <div class="lu-activity">
-    <div class="lu-activity-body tw-bg-bulma-lightest">
-
-        <a href="#" class="">
-          <span class="lu-activity-header">Replied:</span>
-        </a>
-        <span class="tw-italic">{{ $record->subject->body }}</span>
-
-    </div>
-  </div> --}}
-
-{{-- 
-  
-    <div class="tw-">
-      <a href="#" class="">
-        <span class="lu-activity-header">Replied:</span>
-      </a>
-      <span class="tw-italic">{{ $activity->subject->body }}</span>
-    </div>
-    
-     <div class="tw-">
-       {{ $activity->subject->created_at->diffForHumans() }}
-     </div>
-
-  --}}
